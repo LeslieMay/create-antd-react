e5279f7768ceb148d3fdcc053cb1e2e8100baeb3
张煜杰 <zhangyujieswu@163.com>
2017-12-05 16:49:38 +0800

    test-precss

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {render} from "react-dom"
 import "./app.css"
-consol.log("ddd")
+console.log("ddd")
 render(
 	<h1>===</h1>,document.getElementById("root")
 )
--- webpack.common.js
+++ webpack.common.js
@@ -44,22 +44,27 @@ module.exports = function(env){
 			},
 			{
 	        	test: /\.css$/,
-	        	use:(env=="dev")?["style-loader","css-loader",{
-	        		loader:"postcss-loader",
-	        		options: {           // 如果没有options这个选项将会报错 No PostCSS Config found
-		                plugins: (loader) => [
-		                	require('precss'),
-		                    require('autoprefixer'), //CSS浏览器兼容
-		                ]
-		            }
-	        	}
+	        	use:(env=="dev")?[
+	        		{ loader: 'style-loader', options: { sourceMap: true } },
+    				{ loader: 'css-loader', options: { sourceMap: true } },
+    				{
+		        		loader:"postcss-loader",
+		        		options: {           // 如果没有options这个选项将会报错 No PostCSS Config found
+			                plugins: (loader) => [
+			                	require('precss'),
+			                    require('autoprefixer'), //CSS浏览器兼容
+			                ],
+			                sourceMap:true
+			            }
+	        		}
 	        	]:extractTextPlugin.extract({
 	          		fallback: "style-loader",
 	          		use: ['css-loader', {
 	          			loader:"postcss-loader",
 		        		options: {           // 如果没有options这个选项将会报错 No PostCSS Config found
 			                plugins: (loader) => [
-			                    require('autoprefixer')(), //CSS浏览器兼容
+			                	require('precss'),
+			                    require('autoprefixer'), //CSS浏览器兼容
 			                ]
 			            }
 	          		}]
