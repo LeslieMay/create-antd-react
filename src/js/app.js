e19f4b82d73fcdcf89e38db2cd8c2a42b6e5fe92
张煜杰 <zhangyujieswu@163.com>
2017-12-11 15:57:21 +0800

    测试

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import React from "react"
 import {render} from "react-dom"
 import "./app.css"
 import Test from "./text.js"
+
+console.log("ddd")
 render(
 	<Test/>,document.getElementById("root")
 )
